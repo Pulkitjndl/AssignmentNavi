d7c49a23617ae26459015e81b30fdd9eb93336b1
Pulkit Jindal <pulkit.jindal@freecharge.com>
2022-06-23 10:11:56 +0530

    Added comments to new PR

--- app/src/main/java/com/pulkitjndl/naviSample/model/PullRequestApi.kt
+++ app/src/main/java/com/pulkitjndl/naviSample/model/PullRequestApi.kt
@@ -4,7 +4,6 @@ import io.reactivex.Single
 import retrofit2.http.GET
 
 interface PullRequestApi {
-    // sample - https://github.com/Pulkitjndl/AssignmentNavi/pulls?q=is%3Apr+is%3Aclosed
-      @GET("repos/prakashshuklahub/Pull-Request-GithubApi-Demo-App/pulls?state=closed")
+      @GET("repos/Pulkitjndl/AssignmentNavi/pulls?state=closed")
     fun getPullRequest(): Single<List<PullRequest>>
 }
